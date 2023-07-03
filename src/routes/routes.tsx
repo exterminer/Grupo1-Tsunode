2e3caae34cf28a048ba3692aa209122a5f09c86a
igor <igorbragafarais@gmail.com>
2023-07-03 15:33:07 -0300

    fix:MergeBug

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from "react-router-dom"
-import { Login } from "../pages/Login"
-import { Register } from "../pages/Register"
-import { AllNews } from "../pages/AllNews/AllNews"
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+import { AllNews } from "../pages/AllNews/AllNews";
+import { HomeNews } from "../pages/HomeNews";
 
 export const RoutesMain = () => {
-    return (
-        <Routes>
-            <Route>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/Register" element={<Register />}/>
-                <Route path="/allNews" element={<AllNews />} />
-            </Route>
-            <Route>
-
-            </Route>
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route>
+        <Route path="/" element={<HomeNews />}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/allnews" element={<AllNews />} />
+      </Route>
+      <Route></Route>
+    </Routes>
+  );
+};
