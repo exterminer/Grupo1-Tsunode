40483421ebe8c2fd442ce5d8d2d13e678b30aa0b
kalineschroder <kalineschroder@gmail.com>
2023-07-05 11:27:27 -0400

    estilizacao editform

--- src/components/FormEdit/index.tsx
+++ src/components/FormEdit/index.tsx
@@ -6,6 +6,8 @@ import { NewsContext } from "../../providers/NewsContext/NewsContext"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { formEditSchema } from "./formEditSchema"
 
+import arrow from "../../assets/arrowBack.svg"
+
 export const FormEdit = () => {
     const {handleSubmit, register, formState : {errors}} = useForm<IFormEdit>({
         resolver: zodResolver(formEditSchema)
@@ -13,32 +15,33 @@ export const FormEdit = () => {
     const {handleEditNew} = useContext(NewsContext)
    
 const submit = (formData : IFormEdit) => {
-    
-    
     handleEditNew(formData, 1)
     console.log(formData)
 }
 console.log(errors)
     return (
         <>
-    <section className="p-5 bg-grey h-full">
-        <div className="flex flex-row justify-between my-6">
-            <h1 className="text-black text-xl font-bold font-lora text-2xl mb-[15px]">Editando:</h1>
-            <Link to="/">
-                <button className="text-white text-xs text-base font-inter font-bold rounded-md bg-blue w-[77px] h-[34px]">Voltar</button>
-            </Link>
+    <section className="p-5 bg-grey h-full flex flex-col items-center">
+        <div className="w-[800px]">
+            <div className="flex flex-row justify-between items-center mx-auto my-6">
+                <h1 className="text-black text-xl font-bold font-lora text-2xl mb-[15px]">Editando:</h1>
+                <Link to="/" className="flex justify-center items-center gap-3 bg-white w-[107px] h-[43px] border-[1px] border-blue rounded">
+                    <img src={ arrow } alt="arrow Back" className="w-[18px] h-[18px]"/>
+                    <button className="text-blue text-[14px] font-bold ">Voltar</button>
+                </Link>
         </div>
-        <form className="flex flex-col" onSubmit={handleSubmit(submit)}>
-            <label>Título</label>
-            <input {...register("title")} className="border-2 border-black" type="text" placeholder="Digite o título do seu post" />
-            <label>Imagem em destaque</label>
-            <input {...register("image")} className="border-2 border-black" type="url" placeholder="Digite o link da sua imagem" />
-            <label>Conteúdo</label>
-            <textarea {...register("description")} className="border-2 border-black"></textarea>
-            <div className="flex justify-start">
-            <button type="submit"  className="text-white font-bold text-xs text-base font-inter font-bold rounded-md bg-blue h-11 w-32 text-base">Salvar post</button>
-            </div>
-        </form>
+            <form className="flex flex-col" onSubmit={handleSubmit(submit)}>
+                <label className="text-xs font-bold font-lora mb-1">Título</label>
+                <input {...register("title")} className="border-none bg-white h-11 rounded-sm mb-3 p-4" type="text" placeholder="Digite o título do seu post" />
+                <label className="text-xs font-bold font-lora mb-1">Imagem em destaque</label>
+                <input {...register("image")} className="border-none bg-white h-11 rounded-sm mb-3 p-4" type="url" placeholder="Digite o link da sua imagem" />
+                <label className="text-xs font-bold font-lora mb-1">Conteúdo</label>
+                <textarea {...register("description")} className="border-none bg-white rounded-sm mb-8 p-4" rows={10} ></textarea>
+                <div className="flex justify-start">
+                <button type="submit"  className="text-white font-bold text-xs text-base font-inter font-bold rounded-md bg-blue h-11 w-32 text-base">Salvar post</button>
+                </div>
+            </form>
+        </div>
     </section>
         </>
     )
